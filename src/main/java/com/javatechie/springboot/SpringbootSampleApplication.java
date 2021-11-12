318efcb014ee48e6e0b8793ad1a85f1f17afb803
javatechie <basanta.kumar.hota.4@gmail.com>
2021-11-12 18:10:54 +0530

    code

---
+++ src/main/java/com/javatechie/springboot/SpringbootSampleApplication.java
@@ -0,0 +1,45 @@
+package com.javatechie.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/books")
+public class SpringbootSampleApplication {
+
+
+    public List<Book> books = new ArrayList<>();
+
+    @PostConstruct
+    public void initBooks() {
+        books.add(Book.builder().id(101).name("Java").author("James").build());
+    }
+
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        books.add(book);
+        return book;
+    }
+
+    @GetMapping
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSampleApplication.class, args);
+    }
+//    echo "# github-workflow-demo" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/basanta-spring-boot/github-workflow-demo.git
+//    git push -u origin main
+}
